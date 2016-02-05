d8de3524fdfda7d55d8164028df2b6004219363e
hnnesv <venterh@gmail.com>
2016-02-05 16:19:43 +0200

    First basic working version of script

---
+++ update-infoman-facilities.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+import sys
+import getopt
+import requests
+from xml.etree import ElementTree as ET
+
+
+# which column to use for pepfar and local IDs
+PEPFAR_ID_COL=0
+LOCAL_ID_COL=1
+
+DEFAULT_URL="http://localhost:8984/CSD"
+
+
+usage_msg = """Usage: $0 [OPTIONS...] CSV
+Updates OpenInfoMan with facility codes provided by a file in csv format.
+OPTIONS are:
+    -h
+        Print help and exit.
+    -l
+        Treat the first line as a row. Without this option the first line will be treated as a header and ignored.
+    -u
+        The base URL to use for OpenInfoMan. Without this option, the value 'http://localhost:8984/CSD' will be used.
+"""
+
+def print_usage_and_exit():
+    print usage_msg
+    sys.exit()
+
+
+ERROR=0
+SUCCESS=1
+WARN=2
+
+def line_print(line_num, msg="", status=-1):
+    """Print a message for the current line"""
+
+    if status == ERROR:
+        _stat = "\x1b[31mError\x1b[0m"
+    elif status == SUCCESS:
+        _stat = "\x1b[32mSuccess\x1b[0m"
+    elif status == WARN:
+        _stat = "\x1b[33mWarn\x1b[0m"
+    else:
+        _stat = "Info"
+
+    print "[%s] line %s: %s" % (_stat, line_num, msg)
+
+
+def split_csv_line(line):
+    """Split a CSV row on comma delimiters"""
+
+    line = line.rstrip() 
+
+    split = []
+    token = ''
+    in_quotes = 0
+    is_in_quotes = lambda: in_quotes%2 == 1
+
+    for c in line:
+        if c == '"':
+            in_quotes = in_quotes+1
+            continue
+
+        if c == ',' and not is_in_quotes():
+            split.append(token)
+            token = ''
+        else:
+            token = token + c
+
+    if len(token)>0: split.append(token)
+
+    return split
+
+
+FACILITY_SEARCH = "/csr/CSD-Facilities-Connectathon-20150120/careServicesRequest/urn:ihe:iti:csd:2014:stored-function:facility-search"
+FACILITY_UPDATE = "/csr/CSD-Facilities-Connectathon-20150120/careServicesRequest/update/urn:openhie.org:openinfoman:facility_create"
+
+FACILITY_SEARCH_BODY = """
+<requestParams xmlns="urn:ihe:iti:csd:2013">
+  <id entityID="%s"/>
+</requestParams>
+"""
+
+class RequestException(Exception): pass
+class ContentException(Exception): pass
+
+def lookup_csd_facility(base_url, entity_id):
+    request_body = FACILITY_SEARCH_BODY % (entity_id)
+    r = requests.post(base_url+FACILITY_SEARCH, headers={'content-type': 'text/xml'}, data=request_body)
+
+    if r.status_code != 200: raise RequestException('Request to OpenInfoMan responded with status ' + str(r.status_code) + ': ' + r.text)
+
+    root = ET.fromstring(r.text)
+    for child in root:
+        if child.tag.endswith('facilityDirectory'):
+            if len(child) >= 1:
+                return child[0]
+
+    return None
+
+def send_csd_facility_update(base_url, request):
+    r = requests.post(base_url+FACILITY_UPDATE, headers={'content-type': 'text/xml'}, data=request)
+
+    if r.status_code != 200: raise RequestException('Request to OpenInfoMan responded with status ' + str(r.status_code) + ': ' + r.text)
+
+
+def process_facility_update(base_url, pepfar_id, local_id):
+    facility = lookup_csd_facility(base_url, pepfar_id)
+    if facility is None:
+        raise ContentException('Could not find facility with entityID ' + pepfar_id)
+
+    ET.SubElement(facility, 'otherID', {'code': local_id, 'codingSchema': 'TODO'})
+    updateRequest = ET.Element('requestParams')
+    updateRequest.append(facility)
+    requestString = ET.tostring(updateRequest, encoding='utf-8')
+    send_csd_facility_update(base_url, requestString)
+
+def process_csv_contents(args, base_url, read_first_line=False):
+    print "Using OpenInfoMan instance " + base_url
+    print "Processing CSV %s ..." % (args[0])
+
+    with open(args[0], 'r') as f:
+        line_num=0
+        first_line=not read_first_line
+
+        for line in f:
+            if first_line == True:
+                first_line = False
+            else:
+                row = split_csv_line(line)
+
+                if len(row) <= max(PEPFAR_ID_COL, LOCAL_ID_COL) or row[PEPFAR_ID_COL] == '' or row[LOCAL_ID_COL] == '':
+                    line_print(line_num, "invalid content", WARN)
+                else:
+                    try:
+                        process_facility_update(base_url, row[PEPFAR_ID_COL], row[LOCAL_ID_COL])
+                    except ContentException as e:
+                        line_print(line_num, e.message, WARN)
+                    except RequestException as e:
+                        line_print(line_num, e.message, ERROR)
+                        sys.exit(1)
+
+            line_num = line_num+1
+
+
+
+if __name__ == "__main__":
+    base_url=DEFAULT_URL
+    read_first_line = False
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hlu:")
+    except getopt.GetoptError:
+        print_usage_and_exit()
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print_usage_and_exit()
+        elif opt == '-l':
+            read_first_line = True
+        elif opt == '-u':
+            base_url = arg
+
+    if len(args) == 0: print_usage_and_exit()
+    
+    process_csv_contents(args, base_url, read_first_line)
