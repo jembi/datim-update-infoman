bf15041fbe5ce2536b8c97416ee0dd454f4d0130
napiergit <andrew.napier@jembi.org>
2016-05-04 12:26:06 +0200

    OHIE-117: Allow user to set coding schema during install

--- packaging/create-deb.sh
+++ packaging/create-deb.sh
@@ -115,7 +115,7 @@ do
     rm -fr $PKGDIR
     mkdir -p $OIDIR
     mkdir -p $SRCDIR
-    git clone -b https://github.com/jembi/$PKG.git  $SRCDIR
+    git clone https://github.com/jembi/$PKG.git  $SRCDIR
     for CPDIR in "${CPDIRS[@]}"
     do
     	if [ -d "$SRCDIR/$CPDIR" ]; then
